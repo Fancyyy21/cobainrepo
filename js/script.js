a71a600cb677dd303e8099cdcff71e1ca8249f5f
Maulana Imanulhaq <98022263+Fancyyy21@users.noreply.github.com>
2023-10-07 14:02:41 +0700

    json about us

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-function proposal(){
-    window.location.href = "./pages/proposal.html";
-}
+document.getElementById('lihatProposalBtn').addEventListener('click', function() {
+    // Arahkan pengguna ke proposal.html
+    window.location.href = './pages/proposal.html';
+});
